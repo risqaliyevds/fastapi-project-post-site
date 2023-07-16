608502b8fdd92baa289f01e45ab6a2bda21ecc0f
Muradjon Riskaliev <risqaliyevds@gmail.com>
2023-07-16 12:40:19 +0500

    changed users and auth place

--- app/main.py
+++ app/main.py
@@ -15,8 +15,8 @@ app.add_middleware(CORSMiddleware,
                    allow_methods = ["*"],
                    allow_headers = ["*"])
 
-app.include_router(auth.router)
 app.include_router(users.router)
+app.include_router(auth.router)
 app.include_router(posts.router)
 app.include_router(votes.router)
 
